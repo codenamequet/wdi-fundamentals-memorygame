36891bd3e05db438714a810560fbdb8ad5657505
Marquet <Marquet@Generals-MacBook-Air-4.local>
2017-11-20 18:16:40 -0500

    added more game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+    },
+    {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+    rank: "king", 
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+    },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+    }]
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -8,15 +28,18 @@ var checkForMatch = function() {
          alert("Sorry, try again.");
      }
 };
-//Not sure how cardId is an array when it's not defined
+
 //function alerts when there's no parameter, doesn't alert with parameter
 var flipCard = function(cardId) {
     
     if(cardsInPlay.length === 2) {
        checkForMatch(); 
     };
-    console.log("User flipped " + cards[cardId])
-    cardsInPlay.push(cards[cardId])
+    console.log("User flipped " + cards[cardId].rank)
+    console.log(cards[cardId].cardImage)
+    console.log(cards[cardId].suit)
+    cardsInPlay.push(cards[cardId].rank)
+    
 };
 
 flipCard(0);
